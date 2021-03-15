8e3c6f66565d74ba9a803a17afc2664b84016a59
Parkjisu0411 <jisu0411@gmail.com>
2021-03-15 16:43:35 +0900

    init

---
+++ src/test/java/com/gada/travelgada/TravelgadaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gada.travelgada;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelgadaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
