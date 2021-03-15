fcb99053d3cef1a5779a197b8c04a680072386dc
DESKTOP-Q6EJGLL\dongh <donghan1864@naver.com>
2021-03-15 18:18:55 +0900

    실험

--- src/main/java/com/gada/travelgada/TravelgadaApplication.java
+++ src/main/java/com/gada/travelgada/TravelgadaApplication.java
@@ -2,7 +2,7 @@ package com.gada.travelgada;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//커밋????
 @SpringBootApplication
 public class TravelgadaApplication {
 
