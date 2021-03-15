24c302d49d9d433c1b27cee08fc1a73a0a4a6944
Parkjisu0411 <jisu0411@gmail.com>
2021-03-15 20:25:22 +0900

    Update DBConnection unit test, finish  - jisu

--- src/test/java/com/gada/travelgada/web/DBConnectionTest.java
+++ src/test/java/com/gada/travelgada/web/DBConnectionTest.java
@@ -4,7 +4,12 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class DBConnectionTest {
 	
 	private static final String DRIVER = "com.mysql.jdbc.Driver";
