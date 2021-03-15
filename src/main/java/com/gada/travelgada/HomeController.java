ac14b34bf10897eab325c06578fde01e1ebe8437
Parkjisu0411 <jisu0411@gmail.com>
2021-03-15 17:13:19 +0900

    Update init - jisu

---
+++ src/main/java/com/gada/travelgada/HomeController.java
@@ -0,0 +1,13 @@
+package com.gada.travelgada;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+   @RequestMapping("/")
+   public String Home() {
+      return "hello, spring boot";      // 리턴값을 body로 가져다 직접 출력하는 것임.
+   }                              // 따라서 view가 없어도 출력됨.
+}
