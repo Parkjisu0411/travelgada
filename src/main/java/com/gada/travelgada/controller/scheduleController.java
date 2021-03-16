c28b85118a5593797b725ea75e93ba676fa6a6be
DESKTOP-Q6EJGLL\dongh <donghan1864@naver.com>
2021-03-16 17:08:57 +0900

    완전 미완성..

--- src/main/java/com/gada/travelgada/controller/scheduleController.java
+++ src/main/java/com/gada/travelgada/controller/scheduleController.java
@@ -4,11 +4,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class HomeController {
+public class scheduleController {
 
-   @RequestMapping("/")
+   @RequestMapping("/schedule")
    public String Home() {
-      return "hello, spring boot";      // 리턴값을 body로 가져다 직접 출력하는 것임.
+      return "schedule";      // 리턴값을 body로 가져다 직접 출력하는 것임.
    }                              // 따라서 view가 없어도 출력됨.
 
 }
