a06afb34f5923d86fd90b3beb3b5ccfe473894ee
Parkjisu0411 <jisu0411@gmail.com>
2021-04-07 14:55:44 +0900

    충돌 정리

--- src/main/java/com/gada/travelgada/controller/ShoppingController.java
+++ src/main/java/com/gada/travelgada/controller/ShoppingController.java
@@ -1,7 +1,5 @@
 package com.gada.travelgada.controller;
 
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.log;
-
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
